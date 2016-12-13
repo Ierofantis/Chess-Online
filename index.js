9eb985814f2e4acb01fc9874a35b0f8db18f2655
Ierofantis <evil69669@gmail.com>
2016-12-13 14:37:45 +0200

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/public'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+io.on('connection', function(socket){
+	socket.on('toggle', function(msg){
+		io.emit('toggle', msg);
+	});
+	socket.on('move', function(msg){
+		io.emit('move', msg);
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
